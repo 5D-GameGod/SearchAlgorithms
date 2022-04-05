a862e1d6ff0768d405f4caf0ade3d249695bbd3e
5D-GameGod <98898042+5D-GameGod@users.noreply.github.com>
2022-04-05 15:48:48 +0100

    Added binary search function

--- SearchAlgorithms.cpp
+++ SearchAlgorithms.cpp
@@ -33,14 +33,54 @@ void linearSearch( int a[], int n)
 	}
 }
 //------------------------------------------------
+
+//-------------Binary Search Function------------- 
+int binarySearch(int binaryArr[], int left, int right, int n)
+{
+	while(left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if(binaryArr[mid] == n)
+		{
+			return mid;
+		}
+
+		else if(binaryArr[mid] < n)
+		{
+			left = mid + 1;
+		}
+
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return -1;
+}
+
+//------------------------------------------------
 int main()
 {
 	int arr[5] = { 1,2,5,4,5 };
+	int binArray[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
 	cout << "Please enter the number you want to search for" << endl;
 	int num{};
 	cin >> num;
 
 	linearSearch(arr, num);
+	int output{};
+	output = binarySearch(binArray,0,9,num);
+
+	if(output == -1)
+	{
+		cout << "Number was not found" << endl;
+	}
+	else
+	{
+		cout << "Number found at index " << output << endl;
+	}
 
 	return 0;
 }
