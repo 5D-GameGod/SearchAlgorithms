d24c14bc6c514b9563fec718edece475b8460f86
5D-GameGod <98898042+5D-GameGod@users.noreply.github.com>
2022-04-05 15:14:11 +0100

    Linear search function. Used vector to store index of numbers found so that the index position of all numbers found could be printed to the terminal in one line.

--- SearchAlgorithms.cpp
+++ SearchAlgorithms.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
+//-------------Linear Search Function------------- 
+void linearSearch( int a[], int n)
+{
+	vector <int> index{};
+	
+	for(int i{0}; i < 5; i++)
+	{
+		if(a[i] == n)
+		{
+			index.push_back(i);
+		}
+	}
+	if(index.size() == 0)
+	{
+		cout << "Number was not found" << endl;
+	}
+	else if(index.size() == 1)
+	{
+		cout << "Number was found at index " << index.at(0) << endl;
+	}
+	else
+	{
+		cout << "Number was found at index " << index.at(0);
+		for(int i{1}; i < index.size(); i++)
+		{
+			cout << " and " << index.at(i);
+		}
+		cout << "." << endl;
+	}
+}
+//------------------------------------------------
 int main()
 {
-    std::cout << "Hello World!\n";
+	int arr[5] = { 1,2,5,4,5 };
+	cout << "Please enter the number you want to search for" << endl;
+	int num{};
+	cin >> num;
+
+	linearSearch(arr, num);
+
+	return 0;
 }
 
